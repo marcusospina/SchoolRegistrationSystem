4753b11e06ff84e43d8918b8321d8def09789d17
marcusospina <marcus.ospina17@student.qcc.cuny.edu>
2025-12-04 21:33:52 -0500

    Update Main.java

--- src/com/school/app/Main.java
+++ src/com/school/app/Main.java
@@ -1,1 +1,32 @@
+package com.school.app;
+
+import com.school.model.*;
+import com.school.service.*;
+import com.school.view.SchoolSystemUI;
+
+public class Main {
+    public static void main(String[] args) {
+        // instantiate services
+        CourseService courseService = new CourseService();
+        StudentService studentService = new StudentService();
+        InstructorService instructorService = new InstructorService();
+        ClassroomService classroomService = new ClassroomService();
+
+        RegistrationService registrationService = new RegistrationService(
+                instructorService, courseService, classroomService, studentService
+        );
+
+        // UI
+        SchoolSystemUI ui = new SchoolSystemUI(
+                studentService,
+                instructorService,
+                courseService,
+                classroomService,
+                registrationService
+        );
+
+        // run menu loop
+        ui.runMainLoop();
+    }
+}
 
