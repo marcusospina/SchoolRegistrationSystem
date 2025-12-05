90bb197c3bd568cb9637f890978bf8f7f6004238
marcusospina <marcus.ospina17@student.qcc.cuny.edu>
2025-12-04 21:22:32 -0500

    Update Course.java

--- src/com/school/model/Course.java
+++ src/com/school/model/Course.java
@@ -1,1 +1,27 @@
+package com.school.model;
+
+public final class Course {
+    private final String courseId;
+    private final String name;
+    private final int credits;
+
+    public Course(String courseId, String name, int credits) {
+        this.courseId = courseId;
+        this.name = name;
+        this.credits = credits;
+    }
+
+    public String getCourseId() { return courseId; }
+    public String getName() { return name; }
+    public int getCredits() { return credits; }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseId='" + courseId + '\'' +
+                ", name='" + name + '\'' +
+                ", credits=" + credits +
+                '}';
+    }
+}
 
